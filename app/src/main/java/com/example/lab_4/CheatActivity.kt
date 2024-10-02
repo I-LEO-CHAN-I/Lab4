3091f2bd0ca413c86c2efdc0bb774828909b34e6
AAM1337 <amacinov@gmail.com>
2024-10-03 00:17:08 +0700

    Начата работа со второй активностью

--- app/src/main/java/com/example/lab_4/CheatActivity.kt
+++ app/src/main/java/com/example/lab_4/CheatActivity.kt
@@ -1,10 +1,14 @@
 package com.example.lab_4
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+private const val EXTRA_ANSWER_IS_TRUE="com.bignerdranch.android.lab_4.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,14 @@ class CheatActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    companion object{
+        fun newIntent(packageContext: Context,answerIsTrue:Boolean): Intent {
+            return Intent(packageContext,CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue)
+            }
         }
     }
 }
